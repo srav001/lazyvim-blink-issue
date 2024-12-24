716c6d937efa953094553700a44d39eede614c9c
Sravan <sravansuresh1x@gmail.com>
2024-12-24 15:15:58 +0800

    init

---
+++ lua/plugins/example.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "none",
+        ["<C-e>"] = { "hide" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      },
+    },
+  },
+}
